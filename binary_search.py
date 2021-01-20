97eb4d80217221431c3034578549fc7cda01c8ad
James Pemantell <pbin2au@gmail.com>
2021-01-20 07:14:31 -0500

    Committing working version of binary search example

--- binary_search.py
+++ binary_search.py
@@ -1,3 +1,5 @@
+# used example from https://www.w3resource.com/python-exercises/data-structures-and-algorithms/python-search-and-sorting-exercise-1.php
+
 animal_list = ['anteater',
  'antelope',
  'bird',
@@ -11,19 +13,22 @@ animal_list = ['anteater',
  'zebra']
 
 def list_bisect(search_term, list_name):
-    mid = int(len(list_name)/2)+1
-    end = len(list_name)
-    if list_name[mid][0] <= search_term[0]:
-        for i in range(0,mid):
-            if search_term == list_name[i]:
-                return i
-    elif list_name[mid][0] >= search_term[0]:
-        for i in range(mid,end):
-            if search_term == list_name[i]:
-                return i
-    else: 
-        return -1
+    first = 0
+    end = len(list_name)-1
+    found = False
+    search_term_letter = search_term[0]
+    while (first <= end and not found):
+        mid = (first + end)//2
+        reference_name_letter = list_name[mid][0] 
+        if list_name[mid] == search_term:
+            found = True
+            return mid
+        else:
+            if search_term_letter  < reference_name_letter:
+                end = mid - 1
+            elif search_term_letter  > reference_name_letter:
+                first = mid + 1
+            else:
+                return -1
 
-list_bisect('monkey', animal_list)
-
-            
+list_bisect('snake', animal_list)
